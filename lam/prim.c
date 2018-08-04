406e325d920724929c542551a8070c189908904e
bydriv <bydriv@gmail.com>
2018-08-04 09:16:51 +0900

    LAMG_BINOP

--- lam/prim.c
+++ lam/prim.c
@@ -2,6 +2,17 @@
 #include <stdbool.h>
 #include <runtime.h>
 
+#define LAMB_BINOP(type, name, op) \
+  uintptr_t name(uintptr_t env_count, closure_t *env_values, uintptr_t stack_count, closure_t *stack_values) { \
+    env_t env = {env_count, env_values};                                \
+    stack_t stack = {stack_count, stack_values};                        \
+    GRAB(&env, &stack);                                                 \
+    type n = ACCESS(&env, &stack, 0);                                   \
+    GRAB(&env, &stack);                                                 \
+    type m = ACCESS(&env, &stack, 0);                                   \
+    return n op m;                                                      \
+  }
+
 uintptr_t lamb_true(uintptr_t env_count, closure_t *env_values, uintptr_t stack_count, closure_t *stack_values) {
   return (uintptr_t) true;
 }
@@ -36,75 +47,13 @@ uintptr_t lamb_succ64(uintptr_t env_count, closure_t *env_values, uintptr_t stac
   return n + 1;
 }
 
-uintptr_t lamb_eq64(uintptr_t env_count, closure_t *env_values, uintptr_t stack_count, closure_t *stack_values) {
-  env_t env = {env_count, env_values};
-  stack_t stack = {stack_count, stack_values};
-  GRAB(&env, &stack);
-  uintptr_t n = ACCESS(&env, &stack, 0);
-  GRAB(&env, &stack);
-  uintptr_t m = ACCESS(&env, &stack, 0);
-  return n == m;
-}
-
-uintptr_t lamb_lt64(uintptr_t env_count, closure_t *env_values, uintptr_t stack_count, closure_t *stack_values) {
-  env_t env = {env_count, env_values};
-  stack_t stack = {stack_count, stack_values};
-  GRAB(&env, &stack);
-  uintptr_t n = ACCESS(&env, &stack, 0);
-  GRAB(&env, &stack);
-  uintptr_t m = ACCESS(&env, &stack, 0);
-  return n < m;
-}
-
-uintptr_t lamb_add64(uintptr_t env_count, closure_t *env_values, uintptr_t stack_count, closure_t *stack_values) {
-  env_t env = {env_count, env_values};
-  stack_t stack = {stack_count, stack_values};
-  GRAB(&env, &stack);
-  uintptr_t n = ACCESS(&env, &stack, 0);
-  GRAB(&env, &stack);
-  uintptr_t m = ACCESS(&env, &stack, 0);
-  return n + m;
-}
-
-uintptr_t lamb_sub64(uintptr_t env_count, closure_t *env_values, uintptr_t stack_count, closure_t *stack_values) {
-  env_t env = {env_count, env_values};
-  stack_t stack = {stack_count, stack_values};
-  GRAB(&env, &stack);
-  uintptr_t n = ACCESS(&env, &stack, 0);
-  GRAB(&env, &stack);
-  uintptr_t m = ACCESS(&env, &stack, 0);
-  return n - m;
-}
-
-uintptr_t lamb_mul64(uintptr_t env_count, closure_t *env_values, uintptr_t stack_count, closure_t *stack_values) {
-  env_t env = {env_count, env_values};
-  stack_t stack = {stack_count, stack_values};
-  GRAB(&env, &stack);
-  uintptr_t n = ACCESS(&env, &stack, 0);
-  GRAB(&env, &stack);
-  uintptr_t m = ACCESS(&env, &stack, 0);
-  return n * m;
-}
-
-uintptr_t lamb_div64(uintptr_t env_count, closure_t *env_values, uintptr_t stack_count, closure_t *stack_values) {
-  env_t env = {env_count, env_values};
-  stack_t stack = {stack_count, stack_values};
-  GRAB(&env, &stack);
-  uintptr_t n = ACCESS(&env, &stack, 0);
-  GRAB(&env, &stack);
-  uintptr_t m = ACCESS(&env, &stack, 0);
-  return n / m;
-}
-
-uintptr_t lamb_mod64(uintptr_t env_count, closure_t *env_values, uintptr_t stack_count, closure_t *stack_values) {
-  env_t env = {env_count, env_values};
-  stack_t stack = {stack_count, stack_values};
-  GRAB(&env, &stack);
-  uintptr_t n = ACCESS(&env, &stack, 0);
-  GRAB(&env, &stack);
-  uintptr_t m = ACCESS(&env, &stack, 0);
-  return n % m;
-}
+LAMB_BINOP(uint64_t, lamb_eq64, ==)
+LAMB_BINOP(uint64_t, lamb_lt64, <)
+LAMB_BINOP(uint64_t, lamb_add64, +)
+LAMB_BINOP(uint64_t, lamb_sub64, -)
+LAMB_BINOP(uint64_t, lamb_mul64, *)
+LAMB_BINOP(uint64_t, lamb_div64, /)
+LAMB_BINOP(uint64_t, lamb_mod64, %)
 
 uintptr_t lamb_print64(uintptr_t env_count, closure_t *env_values, uintptr_t stack_count, closure_t *stack_values) {
   env_t env = {env_count, env_values};
